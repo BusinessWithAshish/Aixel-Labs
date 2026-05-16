74f9a71153f8a702a12b62e152bb82e7b5145cb2
BusinessWithAshish <businesswithashish.co@gmail.com>
2026-05-16 19:05:38 +0530

    FE: add coming soon pages for Facebook and Instagram Adv search

---
+++ frontend/app/(protected)/lead-generation/facebook/page.tsx
@@ -0,0 +1,11 @@
+import PageLayout from '@/components/common/PageLayout';
+import { ComingSoon } from '@/components/common/ComingSoon';
+
+export default function FacebookLeadGenerationPage() {
+    return (
+        <PageLayout title="Facebook">
+            <ComingSoon />
+        </PageLayout>
+    );
+}
+
---
+++ frontend/app/(protected)/lead-generation/instagram-advanced-search/page.tsx
@@ -0,0 +1,11 @@
+import PageLayout from '@/components/common/PageLayout';
+import { ComingSoon } from '@/components/common/ComingSoon';
+
+export default function InstagramAdvancedSearchLeadGenerationPage() {
+    return (
+        <PageLayout title="Instagram Advanced Search">
+            <ComingSoon />
+        </PageLayout>
+    );
+}
+
