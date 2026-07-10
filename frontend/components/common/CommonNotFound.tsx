bec3feb402d2bce4af018177aace09b293d7a453
BusinessWithAshish <businesswithashish.co@gmail.com>
2026-07-10 16:04:36 +0530

    FE: fix 404 UI issue by replacing next/link with hard navigation link

--- frontend/components/common/CommonNotFound.tsx
+++ frontend/components/common/CommonNotFound.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { DEFAULT_HOME_PAGE_ROUTE } from "@/config/app-config";
 
 export const CommonNotFound = () => {
@@ -7,9 +6,10 @@ export const CommonNotFound = () => {
         <div className="flex flex-col items-center justify-center h-full">
             <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
             <p className="text-lg">The page you are looking for does not exist.</p>
-            <Link href={DEFAULT_HOME_PAGE_ROUTE} className="text-primary">
+            {/* Hard navigation: next/link from not-found.tsx updates the URL but can leave the 404 UI stuck */}
+            <a href={DEFAULT_HOME_PAGE_ROUTE} className="text-primary">
                 Go back to the home page
-            </Link>
+            </a>
         </div>
     );
 };
